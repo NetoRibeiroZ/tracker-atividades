d90549294d1fbfcee6e9ec5258f1c1044e76fac5
Neto Ribeiro <antoniodrigues@hotmail.com>
2023-08-02 10:57:11 -0300

    Alterações nas pastas

---
+++ src/components/TimerCount.vue
@@ -0,0 +1,32 @@
+<template >
+    <div class="ml-3">
+        <section>
+            <strong class="display">
+                {{ timeSpend }}
+            </strong>
+        </section>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: "TimerCount",
+    props:{
+        timeSec:{
+            type: Number,
+            default: 0
+        }
+    },
+    computed: {
+        timeSpend(): string {
+            return new Date(this.timeSec * 1000).toISOString().substr(11, 8)
+        }
+    }
+})
+</script>
+<style >
+.display{
+    color: var(--Text-primary);
+}
+</style>
